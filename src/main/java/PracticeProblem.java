98128215db3fb3600e66c3709b7fa5cfbb862915
Peter-Zheng1 <343277125@tdsb.ca>
2026-03-11 15:49:14 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,40 @@
+/**
+ * Lesson: 2.2
+ * Author: Peter Zheng
+ * Date Created: March 11 2026
+ * Date Last Modified: March 11 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+
+public static boolean isEven(int num) {
+return num % 2 == 0;
+
+}
+
+public static boolean isOdd(int num) {
+return num % 2 != 0;
+}
+
+public static boolean isPositive(int num) {
+return num > 0;
+
+}
+
+public static boolean isNegative(int num) {
+return num < 0;
+
+
+}
+
+public static int combinedLength(String word, String word2) {
+String result = word + word2;
+return result.length();
+
+}
 
 }
